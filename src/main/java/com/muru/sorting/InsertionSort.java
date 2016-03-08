32cf1d772577f2049c1d3762bc875a3e4cdbd42d
Murugavel Sivagnanam <msivagna@cisco.com>
2016-03-08 00:32:56 -0800

    Insertion sort

--- src/main/java/com/muru/sorting/InsertionSort.java
+++ src/main/java/com/muru/sorting/InsertionSort.java
@@ -1,7 +1,32 @@
 package com.muru.sorting;
 
+import java.util.List;
+
 /**
+ * Sort a given list in ascending order using insertion sort
  * Created by msivagna on 3/7/16.
  */
-public class InsertionSort {
+public class InsertionSort<T extends Comparable<T>> {
+    /**
+     * Sort a given list in ascending order
+     * Algorithm
+     *  - All the elements between 0 and i are sorted
+     *  - Take the element i + 1
+     *  - Find an appropriate place in the sorted sublist lets say k
+     *  - Shift all the elements to the right until i + 1
+     *  - Insert the i + 1 th element at kth position
+     *  - Do this until the end of the list
+     * @param inputList
+     */
+    public void sort(List<T> inputList) {
+        for (int i = 0; i < inputList.size() - 1; i++) {
+            T elementCurrentlySorted = inputList.get(i + 1);
+            int position = i + 1;
+            for (int j = i; j >= 0 && elementCurrentlySorted.compareTo(inputList.get(j)) <= 0; j--) {
+                inputList.set(j+1, inputList.get(j));
+                position = j;
+            }
+            inputList.set(position, elementCurrentlySorted);
+        }
+    }
 }
---
+++ src/test/java/com/muru/sorting/InsertionSortTest.java
@@ -0,0 +1,62 @@
+package com.muru.sorting;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * Created by msivagna on 3/7/16.
+ */
+public class InsertionSortTest {
+    public InsertionSort<Integer> insertionSort;
+
+    @BeforeClass
+    public void setup() {
+        insertionSort = new InsertionSort<Integer>();
+    }
+
+    private List<Integer> generateRandomList() {
+        //Generate a list of random numbers between 0 and 9
+        List<Integer> numbers = new ArrayList<Integer>();
+        for (int i = 0; i < 10; i++) {
+            numbers.add(i);
+        }
+        Collections.shuffle(numbers);
+        return numbers;
+    }
+
+    @Test
+    public void mustSuccessfullySortARandomlySortedList() {
+        List<Integer> numbers = generateRandomList();
+        List<Integer> expectedSortedNumbers = new ArrayList<Integer>(numbers);
+        Collections.sort(expectedSortedNumbers);
+        insertionSort.sort(numbers);
+        assertEquals(numbers, expectedSortedNumbers);
+    }
+
+    @Test
+    public void mustSuccessfullySortAPreSortedList() {
+        List<Integer> numbers = generateRandomList();
+        List<Integer> expectedSortedNumbers = new ArrayList<Integer>(numbers);
+        Collections.sort(expectedSortedNumbers);
+        Collections.sort(numbers);
+        insertionSort.sort(numbers);
+        assertEquals(numbers, expectedSortedNumbers);
+    }
+
+    @Test
+    public void mustSuccessfullySortAReverseSortedList() {
+        List<Integer> numbers = generateRandomList();
+        List<Integer> expectedSortedNumbers = new ArrayList<Integer>(numbers);
+        Collections.sort(expectedSortedNumbers);
+        Collections.sort(numbers);
+        Collections.reverse(numbers);
+        insertionSort.sort(numbers);
+        assertEquals(numbers, expectedSortedNumbers);
+    }
+}
